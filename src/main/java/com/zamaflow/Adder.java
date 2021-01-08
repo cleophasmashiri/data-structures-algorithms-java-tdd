d534d6fc0e989378d420cb7f512a63a70cb1fe56
cleophas <cleophasmashiri@gmail.com>
2021-01-08 14:16:32 +0200

    Updated test

---
+++ src/main/java/com/zamaflow/Adder.java
@@ -0,0 +1,29 @@
+package com.zamaflow;
+
+import java.util.Stack;
+
+public class Adder {
+
+    public Stack<Integer> sum(Stack<Integer> s1, Stack<Integer> s2) {        
+        int sum = convertToNumber(s1) + convertToNumber(s2);
+        return convertToStack(sum);
+    }
+
+    Stack<Integer> convertToStack(int n) {
+        String s = String.valueOf(n);
+        Stack<Integer> ss = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            ss.push(Integer.valueOf(s.substring(i, i+1)));
+        }
+        return ss;
+    }
+
+    int convertToNumber(Stack<Integer> s) {
+        StringBuilder b = new StringBuilder();
+        while (!s.empty()) {
+            b.append(s.pop());
+        }
+        return Integer.parseInt(b.toString());
+    }
+
+}
--- src/test/java/com/zamaflow/LinkedListTests.java
+++ src/test/java/com/zamaflow/LinkedListTests.java
@@ -2,6 +2,8 @@ package com.zamaflow;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Stack;
+
 import org.junit.Test;
 
 public class LinkedListTests {
@@ -17,8 +19,49 @@ public class LinkedListTests {
         ll.removeDuplicates();
 
         assertEquals(ll.size(), 3);
+    }
 
+    @Test
+    public void givenStackMustReturnIntEquivalence() {
+        Adder adder = new Adder();
+        Stack<Integer> s1 = new Stack<>();
+        s1.push(8);
+        s1.push(2);
+        s1.push(5);
+        assertEquals(adder.convertToNumber(s1), 528);
+    }
 
+    @Test
+    public void givenIntMustReturnStackEquivalence() {
+        Adder adder = new Adder();
+        int n = 528;
+        Stack<Integer> s  = adder.convertToStack(n);
+        assertEquals(s.pop(), Integer.valueOf(8));
+        assertEquals(s.pop(), Integer.valueOf(2));
+        assertEquals(s.pop(), Integer.valueOf(5));
+    }
+
+    @Test
+    public void givenTwoNumberAsLinkedListShouldComputeSum() {
+        Adder adder = new Adder();
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        Stack<Integer> sum = new Stack<>();
+        s1.push(8);
+        s1.push(2);
+        s1.push(5);
+
+        s2.push(8);
+        s2.push(2);
+        s2.push(2);
+
+        sum.push(7);
+        sum.push(5);
+        sum.push(6);
+
+        // sum = 528 + 228 = 756
+
+        assertEquals(adder.sum(s1, s2), sum);
     }
     
 }
